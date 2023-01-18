fcd2d42827305c3b74b2bae1900f30322d72b75c
AlanPan0131 <panalan0131@gmail.com>
2023-01-18 13:42:59 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-module.exports = {
-    images: {
-        domains: ['scdn.line-apps.com', 'qr-official.line.me'],
-        loader: "imgix",
-        path: "/"
-    },
+/*
+export const PHASE_EXPORT = 'phase-export'
+export const PHASE_PRODUCTION_BUILD = 'phase-production-build'
+export const PHASE_PRODUCTION_SERVER = 'phase-production-server'
+export const PHASE_DEVELOPMENT_SERVER = 'phase-development-server'
+export const PHASE_TEST = 'phase-test'
+*/
+
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+    const nextConfig = {
+        images: {
+            domains: ['scdn.line-apps.com', 'qr-official.line.me']
+        }
+    };
+    if (phase === PHASE_PRODUCTION_BUILD) { // run dev
+        nextConfig.images.loader = 'imgix';
+        nextConfig.images.path = '/';
+    }
+    return nextConfig;
 }
---
+++ pages/line/setting.js
@@ -0,0 +1,82 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+import demo1 from '../images/portrait.png'
+
+import NavComponent from '../components/NavComponent';
+import FooterComponent from '../components/FooterComponent';
+
+import HistoryData from '../data/history.json';
+import FunctionsData from '../data/functions.json';
+
+export default function LineSetting() {
+  return (
+    <div>
+      <Head>
+        <title>每日文大</title>
+      </Head>
+      <NavComponent></NavComponent>
+      <section id={styles.cover} className={styles.section}>
+        <div className={styles['left-block']}>
+          <h1>提供各項最新即時資訊<br />的LINE BOT機器人</h1>
+          <p className={styles['top-btn']}> ▼ 全功能免費，快速加入 </p>
+          <Link href="/#add_friend" class="btn-primary">加入好友</Link>
+        </div>
+        <div className={styles['right-block']}>
+          <Image src={demo1} alt="" width={1314} height={2661}></Image>
+        </div>
+      </section>
+      <section id={styles.functions} className={styles.section}>
+        <div className={"table center c2".split(' ').map(s => styles[s]).join(' ')}>
+          {FunctionsData.map(func => {
+            return (<div>
+              <div dangerouslySetInnerHTML={{ __html: func.icon }}></div>
+              <h3>
+                <p>{func.title}</p>
+              </h3>
+              <p>{func.description}</p>
+            </div>)
+          })}
+        </div>
+      </section>
+      <section id='add_friend' className={"section add_friend".split(' ').map(s => styles[s]).join(' ')}>
+        <h2>如何加入</h2>
+        <div className={"table between c3".split(' ').map(s => styles[s]).join(' ')}>
+          <div>
+            <h3>1. 點擊按鈕</h3>
+            <a href="https://lin.ee/h0kZmTc">
+              <Image src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" width={232} height={72} alt="加入每日文大好友" border="0"></Image>
+            </a>
+          </div>
+          <div>
+            <h3>2. 掃描QR code</h3>
+            <Image src="https://qr-official.line.me/sid/M/037gujtt.png" width={180} height={180} alt="加入每日文大好友"></Image>
+          </div>
+          <div>
+            <h3>3. 輸入LINE ID</h3>
+            <p>line主頁右上方加入好友 &gt; 右上方搜尋 &gt; 選擇id &gt; 輸入:<span>@037gujtt</span></p>
+          </div>
+        </div>
+      </section>
+      <section id={styles.history} className={styles.section}>
+        <h2>重大事件</h2>
+        <table cellspacing="0" cellpadding="0">
+          <tbody>
+            {HistoryData.map(history => {
+              return (
+                <tr>
+                  <td>{history.time}</td>
+                  <td>{history.description}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
+      <FooterComponent></FooterComponent>
+    </div>
+  )
+}
