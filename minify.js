ba89ab794b617929c8249b0fb50094afd94aeef6
AlanPan0131 <panalan0131@gmail.com>
2023-03-25 14:30:51 +0800

    update

---
+++ minify.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+function readFiles(dirname, onFileContent, onError) {
+    fs.readdir(dirname, (err, filenames) => {
+        if (err) {
+            onError(err);
+            return;
+        }
+        filenames.forEach(filename => {
+            if (fs.lstatSync(dirname + filename).isDirectory()) {
+                readFiles(dirname + filename + '/', onFileContent, onError);
+            } else {
+                fs.readFile(dirname + filename, 'utf-8', (err, content) => {
+                    if (err) {
+                        onError(err);
+                        return;
+                    }
+                    onFileContent(dirname + filename, content);
+                });
+            }
+        });
+    });
+}
+
+readFiles('./out/_next/static/chunks/pages/', (path, content) => {
+    console.log(path)
+    content = content.replace(/\/\*([\s\S]*?)\*\//gi, '');
+    fs.writeFileSync(path, content);
+},
+    function (err) {
+        throw err;
+    });
