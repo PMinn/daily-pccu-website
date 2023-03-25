25f32444e66c026b722fbe0f73468ab6a9cc3826
AlanPan0131 <panalan0131@gmail.com>
2023-03-25 13:47:24 +0800

    update

--- pages/line/settings/weather.js
+++ pages/line/settings/weather.js
@@ -9,6 +9,7 @@ export default function LineSetting({ }) {
     <div>
       <Head>
         <title>每日文大 | 設定</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link rel="stylesheet" href="/css/LineSetting.css" />
       </Head>
       <SettingsHeaderComponent title="天氣"></SettingsHeaderComponent>
