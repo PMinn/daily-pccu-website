3912e58e4daf59f4c0836e5a0e738e80416b0437
AlanPan0131 <panalan0131@gmail.com>
2023-05-06 00:11:26 +0800

    update

--- pages/course/index.js
+++ pages/course/index.js
@@ -4,6 +4,18 @@ import colleges from './colleges.json';
 import Link from 'next/link';
 
 export default function Course() {
+
+    function openCollapse(e) {
+        var collapseLabel = e.target;
+        if (collapseLabel.classList.contains('open')) {
+            collapseLabel.classList.remove('open');
+            document.getElementById(collapseLabel.dataset.for).classList.remove('open');
+        } else {
+            collapseLabel.classList.add('open');
+            document.getElementById(collapseLabel.dataset.for).classList.add('open');
+        }
+    }
+
     return (
         <div>
             <Head>
@@ -11,7 +23,7 @@ export default function Course() {
                 <link rel="stylesheet" href="/css/course.css" />
             </Head>
             <section className="menu">
-                <div className="collapse-label" data-for="y_109">109學年
+                <div className="collapse-label" data-for="y_109" onClick={openCollapse}>109學年
                     <svg xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 256 256"><path d="M236.8,188.09,149.35,36.22a24.76,24.76,0,0,0-42.7,0L19.2,188.09a23.51,23.51,0,0,0,0,23.72A24.34,24.34,0,0,0,40.55,224h174.9a24.34,24.34,0,0,0,21.33-12.19A23.51,23.51,0,0,0,236.8,188.09ZM222.93,203.8a8.5,8.5,0,0,1-7.48,4.2H40.55a8.5,8.5,0,0,1-7.48-4.2,7.59,7.59,0,0,1,0-7.72L120.52,44.21a8.75,8.75,0,0,1,15,0l87.45,151.87A7.59,7.59,0,0,1,222.93,203.8Z"></path></svg>
                 </div>
                 <div className="collapse-area" id='y_109'>
@@ -30,7 +42,6 @@ export default function Course() {
                 <div id="blocks">
                 </div>
             </section>
-            <script type="module" src="/js/course.js"></script>
         </div>
     )
 }
