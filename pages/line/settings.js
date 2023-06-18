b756cf0f9ffb00976e7272d7e57ba4ea9c67d931
AlanPan0131 <panalan0131@gmail.com>
2023-06-19 03:07:01 +0800

    update

--- pages/line/settings.js
+++ pages/line/settings.js
@@ -1,67 +1,87 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import React, { useEffect, useState } from 'react';
-import functions from '../../data/functions.json';
-import { database } from '../../js/firebaseConfig.js';
-import { ref, get } from "firebase/database";
 
-export default function LineSetting({ }) {
-  const settingsFunction = functions.filter(f => f.setting);
+import functions from '../../data/functions.json';
+
+import { app } from '../../js/firebaseConfig.js';
+import { getDatabase, ref, get } from "firebase/database";
+
+
+export default function Settings() {
+  const functionNames = functions.filter(f => f.setting);
   const [view, setView] = useState("weather");
-  const changeView = e => {
-    let target = e.target;
-    while (!target.classList.contains('setting-li')) {
-      target = target.parentElement;
-    }
-    setView(target.dataset.id);
-    e.stopPropagation();
+  const [weatherLocations, setWeatherLocations] = useState([]);
+
+  const database = getDatabase(app);
+
+  function locationOnClick(location) {
+    setWeatherLocations(weatherLocations.map(wl => {
+      if (wl.location == location) wl.checked = !wl.checked;
+      return wl;
+    }));
   }
 
   useEffect(() => {
+    get(ref(database, 'weatherLocations/'))
+      .then(snapshot => snapshot.val())
+      .then(weatherLocations => weatherLocations.map(l => ({ ...l, checked: false })))
+      .then(wl => setWeatherLocations(wl));
   }, []);
 
   return (
     <div>
       <Head>
-        <title>每日文大 | 設定</title>
+        <title>設定 | 每日文大</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-        <link rel="stylesheet" href="/css/LineSetting.css" />
+        <link rel="stylesheet" href="/css/line/settings.css" />
       </Head>
       <h4 class="header">設定</h4>
       <div class="setting-scroll">
         {
-          settingsFunction.map(f => {
+          functionNames.map(functionName => {
             return (
-              <div className={"setting-li " + (view == f.id ? "inView" : "")} onClick={changeView} data-id={f.id} key={f.id}>
-                <div class="icon" dangerouslySetInnerHTML={{ __html: f.icon }}></div>
-                <div class="title">{f.title}</div>
+              <div className={'setting-li' + (view == functionName.id ? ' inView' : '')} onClick={() => setView(functionName.id)} key={functionName.id}>
+                <div className="icon" dangerouslySetInnerHTML={{ __html: functionName.icon }}></div>
+                <div className="title">{functionName.title}</div>
               </div>
             )
           })
         }
       </div>
-      <div class="views-out">
-        <div class={"view " + (view == "weather" ? "inView" : "")} id="weather_view">
-          <div class="search-bar">
+      <div className="views-out">
+        <div className={"view" + (view == "weather" ? " inView" : "")} id="weather_view">
+          <div className="search-bar">
             <input type="text" name="search" id="search" placeholder="搜尋" />
           </div>
-          <div class="location checked" style={{ opacity: 0.5 }}>
-            <div class="label">
+          <div className="location checked" style={{ opacity: 0.5 }}>
+            <div className="label">
               <div>大義館7F</div>
               <div>文化大學</div>
             </div>
           </div>
+          {
+            weatherLocations.map(weatherLocation => {
+              return (
+                <div className={"location" + (weatherLocation.checked ? " checked" : "")} onClick={() => locationOnClick(weatherLocation.location)}>
+                  <div className="label">
+                    <div>{weatherLocation.location}</div>
+                    <div>{weatherLocation.city}</div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
-        <div class={"view " + (view == "eat" ? "inView" : "")} id="eat_view">
-          <div class="switch-block">
-            <div class="switch">
-              <div class="switch-circle checked" id='switch_default'>預設</div>
-              <div class="switch-circle" id='switch_custom'>自訂</div>
+        <div className={"view" + (view == "eat" ? " inView" : "")} id="eat_view">
+          <div className="switch-block">
+            <div className="switch">
+              <div className="switch-circle checked" id='switch_default'>預設</div>
+              <div className="switch-circle" id='switch_custom'>自訂</div>
             </div>
           </div>
         </div>
       </div>
-      <Script src='/js/line-setting.js' type='module'></Script>
+      {/* <Script src='/js/line-setting.js' type='module'></Script> */}
     </div>
   )
 }
