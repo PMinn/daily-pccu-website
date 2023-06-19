222fbd98bf0fe79a26e59bc98f50df75d410ae93
AlanPan0131 <panalan0131@gmail.com>
2023-06-19 14:27:33 +0800

    update

--- pages/line/settings.js
+++ pages/line/settings.js
@@ -6,27 +6,35 @@ import functions from '../../data/functions.json';
 import { app } from '../../js/firebaseConfig.js';
 import { getDatabase, ref, get } from "firebase/database";
 
-export default function Settings() {
+import ConfirmComponent from '../../components/confirmComponent.js';
+
+export default function Settings({ fontClass }) {
   const functionNames = functions.filter(f => f.setting);
   const [view, setView] = useState("weather");
   const [weatherLocations, setWeatherLocations] = useState([]);
-
-  var numberOfLocationsChecked = 0;
+  const [locationsOverflow, setLocationsOverflow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [isEatCustom, setIsEatCustom] = useState(false);
+  const [customValue, setCustomValue] = useState("");
 
   const database = getDatabase(app);
+
+  function getNumberOfLocationsChecked() {
+    return weatherLocations.filter(wl => wl.checked).length;
+  }
 
   function locationOnClick(location) {
     setWeatherLocations(weatherLocations.map(wl => {
       if (wl.location == location) {
-        if (wl.checked) {
-          numberOfLocationsChecked--;
-        } else if (numberOfLocationsChecked >= 12) {
-          alert("最多只能選擇12個地點");
-          return wl;
+        if (!wl.checked) {
+          if (getNumberOfLocationsChecked() >= 11) {
+            setLocationsOverflow(true);
+          } else {
+            wl.checked = !wl.checked;
+          }
         } else {
-          numberOfLocationsChecked++;
+          wl.checked = !wl.checked;
         }
-        wl.checked = !wl.checked;
       }
       return wl;
     }));
@@ -46,8 +54,9 @@ export default function Settings() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link rel="stylesheet" href="/css/line/settings.css" />
       </Head>
-      <h4 class="header">設定</h4>
-      <div class="setting-scroll">
+      <ConfirmComponent title={""} content={"最多只能選擇12個地點"} show={locationsOverflow} btn={["好"]} onClick={[() => setLocationsOverflow(false)]}></ConfirmComponent>
+      <h4 className="header">設定</h4>
+      <div className="setting-scroll">
         {
           functionNames.map(functionName => {
             return (
@@ -62,7 +71,7 @@ export default function Settings() {
       <div className="views-out">
         <div className={"view" + (view == "weather" ? " inView" : "")} id="weather_view">
           <div className="search-bar">
-            <input type="text" name="search" id="search" placeholder="搜尋" />
+            <input type="text" name="search" id="search" placeholder="搜尋" onInput={e => setSearchValue(e.target.value)} />
           </div>
           <div className="location checked" style={{ opacity: 0.5 }}>
             <div className="label">
@@ -71,9 +80,9 @@ export default function Settings() {
             </div>
           </div>
           {
-            weatherLocations.map(weatherLocation => {
+            weatherLocations.map((weatherLocation, index) => {
               return (
-                <div className={"location" + (weatherLocation.checked ? " checked" : "")} onClick={() => locationOnClick(weatherLocation.location)}>
+                <div className={"location" + (weatherLocation.checked ? " checked" : "")} style={{ display: (weatherLocation.location.includes(searchValue) || weatherLocation.city.includes(searchValue) ? '' : 'none') }} onClick={() => locationOnClick(weatherLocation.location)} key={'weatherLocation_' + index}>
                   <div className="label">
                     <div>{weatherLocation.location}</div>
                     <div>{weatherLocation.city}</div>
@@ -84,11 +93,16 @@ export default function Settings() {
           }
         </div>
         <div className={"view" + (view == "eat" ? " inView" : "")} id="eat_view">
-          <div className="switch-block">
-            <div className="switch">
-              <div className="switch-circle checked" id='switch_default'>預設</div>
-              <div className="switch-circle" id='switch_custom'>自訂</div>
-            </div>
+          <div className="switch">
+            <div className={"switch-circle " + (isEatCustom ? "" : "checked")} onClick={() => setIsEatCustom(false)}>預設</div>
+            <div className={"switch-circle " + (isEatCustom ? "checked" : "")} onClick={() => setIsEatCustom(true)}>自訂</div>
+          </div>
+          <div className='custom-value'>
+            <label className="input-group" htmlFor="custom_value">
+              <input type="text" className={fontClass} placeholder=" " onInput={e => setCustomValue(e.target.value)} value={customValue} id='custom_value' />
+              <div className='label'>自訂選項</div>
+              <div className="note">選項間請使用半形逗號(,)分隔</div>
+            </label>
           </div>
         </div>
       </div>
