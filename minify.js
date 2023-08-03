919f9baf44f9e178b89a12b4ceceed857063656b
AlanPan0131 <panalan0131@gmail.com>
2023-08-03 22:13:17 +0800

    update

--- minify.js
+++ minify.js
@@ -1,7 +1,10 @@
-var fs = require('fs');
+const fs = require('fs');
 
 const querystring = require('querystring');
 const https = require('https');
+
+const minify = require('@node-minify/core');
+const gcc = require('@node-minify/google-closure-compiler');
 
 function readFiles(dirname, onFileContent, onError) {
     fs.readdir(dirname, (err, filenames) => {
@@ -25,52 +28,22 @@ function readFiles(dirname, onFileContent, onError) {
     });
 }
 
-function minify(content) {
-    return new Promise((resolve, reject) => {
-        var chunk = '';
-        const query = querystring.stringify({
-            input: content
-        });
-        const req = https.request({
-            method: 'POST',
-            hostname: 'www.toptal.com',
-            path: '/developers/javascript-minifier/api/raw',
-        }, function (res) {
-            res.setEncoding('utf8');
-            res.on('data', d => {
-                chunk += d;
-            });
-            res.on('end', () => {
-                if (res.statusCode !== 200) reject(JSON.parse(chunk));
-                else resolve(chunk);
-            });
-        });
-        req.on('error', function (err) {
-            reject(err);
-        });
-        req.setHeader('Content-Type', 'application/x-www-form-urlencoded');
-        req.setHeader('Content-Length', query.length);
-        req.end(query, 'utf8');
-    })
-}
-
 readFiles('./out/_next/static/chunks/', (path, content) => {
-    minify(content)
-        .then(minified => {
-            console.log(path)
-            fs.writeFileSync(path, minified);
-        })
-        .catch(err => {
-            console.error(err);
-        })
-}, function (err) {
-    console.error(err);
-});
+    minify({
+        compressor: gcc,
+        input: path,
+        output: path,
+        callback: function (err, min) {
+            if (err) console.error(err);
+            else console.log(path);
+        }
+    });
+}, err => console.error(err));
 
 readFiles('./out/_next/static/css/', (path, content) => {
-    console.log(path)
-    content = content.replace(/\/\*([\s\S]*?)\*\//gi, '');
-    fs.writeFileSync(path, content);
-}, function (err) {
-    console.error(err);
-});
+    var minified = content.replace(/\/\*([\s\S]*?)\*\//gi, '');
+    fs.writeFile(path, minified, err => {
+        if (err) console.error(err);
+        else console.log(path);
+    });
+}, err => console.error(err));
